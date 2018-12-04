d4b74290c3669d2c9133f7739b4fe2597c2f44f2
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-04 13:30:34 +0100

    Use combineReducers

--- script.jsx
+++ script.jsx
@@ -44,18 +44,11 @@ const visibilityFilter = (
   }
 };
 
-const todoApp = (state = {}, action) => {
-  return {
-    todos: todos(
-      state.todos,
-      action
-    ),
-    visibilityFilter: visibilityFilter(
-      state.visibilityFilter,
-      action
-    )
-  }
-}
+const { combineReducers } = Redux;
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
 
 const { createStore } = Redux;
 const store = createStore(todoApp);
