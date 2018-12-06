ccaa62ac8ce55238f8dafa9191adbf92b2983a11
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-06 15:14:49 +0100

    Generating containers with connect() function (FooterLink)

--- script.jsx
+++ script.jsx
@@ -73,43 +73,35 @@ const Link = ({
   );
 };
 
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
+const mapStateToLinkProps = (
+  state,
+  ownProps
+) => {
+  return {
+    active: 
+      ownProps.filter ===
+      state.visibilityFilter
   }
+};
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={
-          props.filter === state.visibilityFilter
-        }
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }
-      >
-        {props.children}
-      </Link>
-    )
-  }
+const mapDispatchToToLinkProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      });
+    }
+  };
 }
 
-FilterLink.contextTypes = {
-  store: React.PropTypes
-};
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToToLinkProps
+)(Link);
 
 const Footer = () => {
   return(
