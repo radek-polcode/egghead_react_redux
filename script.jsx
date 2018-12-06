0ddbb5ec34bca90c640f0026405e2b566c75d77c
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-06 13:53:11 +0100

    Pass the store down with <Provider>

--- script.jsx
+++ script.jsx
@@ -264,20 +264,7 @@ const TodoApp = () => (
   </div>
 );
 
-class Provider extends Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-  render() {
-    return this.props.children;
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
-
+const { Provider } = ReactRedux;
 const { createStore } = Redux;
 
 ReactDOM.render(
